ef5e6806baf2f158aaa87362c7e551cef26495a3
Shyam Kumar <syamkumar@cybersapient.io>
2024-06-06 20:58:43 +0530

    initial commit :tada:

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,9 @@
+const headerNavLinks = [
+  { href: '/', title: 'home' },
+  { href: '/blog', title: 'blog' },
+  { href: '/tags', title: 'tags' },
+  { href: '/projects', title: 'projects' },
+  { href: '/about', title: 'about' },
+]
+
+export default headerNavLinks
