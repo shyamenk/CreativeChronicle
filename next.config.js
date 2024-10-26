e7a97919f311d2097ca41e16b6fe11003e749a90
syam <syamkumar.vm@cybersapient.io>
2024-10-26 19:32:41 +0530

    fix:

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is cloud.umami.is vercel.live;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;
@@ -15,7 +15,6 @@ const ContentSecurityPolicy = `
   font-src 'self';
   frame-src giscus.app
 `
-
 const securityHeaders = [
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
   {
