f312444fac79fb1b44c1d62eeab08b0fab8b78d2
syam <syamkumar.vm@cybersapient.io>
2024-11-19 19:02:27 +0530

    remove dark

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,8 +67,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <link rel="manifest" href="/static/favicons/site.webmanifest" />
       <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
       <meta name="msapplication-TileColor" content="#000000" />
-      <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
-      <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
+      {/* <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" /> */}
+      {/* <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" /> */}
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <Script
         src="https://cloud.umami.is/script.js"
@@ -77,7 +77,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         strategy="afterInteractive"
       />
       <body
-        className={`${openSans.variable} bg-white pl-[calc(100vw-100%)] font-sans text-black antialiased dark:bg-[#191B28] dark:text-white`}
+        className={`${openSans.variable} bg-white pl-[calc(100vw-100%)] font-sans text-black antialiased`}
       >
         <ThemeProviders>
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
